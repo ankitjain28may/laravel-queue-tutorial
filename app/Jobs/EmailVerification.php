cf6c9a4f667134cafb62d92fc27ab1397bd05a88
ankitjain28may <ankitjain28may77@gmail.com>
2020-02-27 03:46:58 +0530

    Updated laravel

--- app/Jobs/EmailVerification.php
+++ app/Jobs/EmailVerification.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Mail\EmailVerification as EmailVerificationMailable;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Carbon;
 
